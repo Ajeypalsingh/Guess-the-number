6acc695b4ee3ea7fb086e82d0788bc5cb15d02ef
Ajaypal Singh <122804961+Ajeypalsingh@users.noreply.github.com>
2023-03-13 11:51:31 -0500

    addind event

---
+++ assets/script/index.js
@@ -0,0 +1,11 @@
+"use strict";
+
+// GEtting Elemnets
+
+const guess = document.querySelector(".guess");
+const guessBtn = document.querySelector(".check");
+const playAgain = document.querySelector(".again");
+const message = document.querySelector(".message");
+const score = document.querySelector(".score");
+const highScore = document.querySelector(".highscore");
+const randomNumber = document.querySelector(".number");
